32ae02129e5e5c691d765eeeda824c513c4fb1be
BurakKocak <burakkocak1999@gmail.com>
2023-04-02 15:52:04 +0300

    Refactoring and Partial Sign Up

--- StoriesPlatfrom/socialnetworkplatform/core/models.py
+++ StoriesPlatfrom/socialnetworkplatform/core/models.py
@@ -7,13 +7,17 @@ User = get_user_model()
 #This is the first class created according to the UML Class Diagram given in Wiki
 # Some of the fileds to be added in the future
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    Name = models.TextField()
-    Surname = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Name_Surname = models.TextField()
     Username = models.TextField()
-    Password = models.TextField()
+    emailaddress = models.CharField(max_length=40, blank=False, default='email@email.com')
+    Password = models.CharField(max_length=15, blank=False, default='password')
+    Biography = models.TextField()
     #Stories to be created
     #Followers to be created
 
     def __str__(self):
         return self.Username
+
+
+
--- StoriesPlatfrom/socialnetworkplatform/core/views.py
+++ StoriesPlatfrom/socialnetworkplatform/core/views.py
@@ -6,4 +6,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'signin.html')
+
+
+#This function is for signup
+def signup(request):
+    if request.method == 'POST':
+        variablelist = [request.POST.get('Name Surname', False), request.POST['username'], request.POST['E-Mail address'],
+                        request.POST['Password']]
+
+        print(variablelist)
+    else:
+        return render(request, 'signup.html')
+
